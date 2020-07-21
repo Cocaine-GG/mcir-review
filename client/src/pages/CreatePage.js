a6f2d97e7af4995ba573a97e025fc8cea1d53380
Daniel Kondratiuk <daniel.kondratiuk.myclientisrich@gmail.com>
2020-07-21 16:46:27 +0200

    Added eventSubmit for CreatePage

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -14,16 +14,16 @@ export const CreatePage = () => {
   const changeHandler = event =>{
     setLinkForm({ ...linkForm, [event.target.name]: event.target.value })
   }
-  const pressHandler = async event =>{
-    if(event.key==='Enter'){
-      try {
-        const data = await request('/api/link/generate', 'POST', {linkForm}, {
-          Authorization : `Bearer ${auth.token}`
-        })
-        history.push(`/detail/${data.link.code}`)
-      }catch (e) {
 
-      }
+  const submitHandler = async event => {
+    event.preventDefault()
+    try {
+      const data = await request('/api/link/generate', 'POST', {linkForm}, {
+        Authorization : `Bearer ${auth.token}`
+      })
+      history.push(`/detail/${data.link.code}`)
+    }catch (e) {
+
     }
   }
 
@@ -31,26 +31,29 @@ export const CreatePage = () => {
     <div className="row">
       <h1 className="col-12">Create Link</h1>
       <div className="col-12 d-flex justify-content-between">
-        <input
-          id="client"
-          name="client"
-          className="form-control"
-          placeholder="Entrez le nom du client"
-          value={linkForm.client}
-          onChange={changeHandler}
-          onKeyPress={pressHandler}
-          required
-        />
-        <input
-          id="project"
-          name="project"
-          className="form-control ml-2"
-          placeholder="Entrez le nom du projet"
-          value={linkForm.project}
-          onChange={changeHandler}
-          onKeyPress={pressHandler}
-          required
-        />
+        <form
+          onSubmit={submitHandler}
+          className="d-flex col-12 px-0">
+          <input
+            id="client"
+            name="client"
+            className="form-control"
+            placeholder="Entrez le nom du client"
+            value={linkForm.client}
+            onChange={changeHandler}
+            required
+          />
+          <input
+            id="project"
+            name="project"
+            className="form-control ml-2"
+            placeholder="Entrez le nom du projet"
+            value={linkForm.project}
+            onChange={changeHandler}
+            required
+          />
+          <button className="btn btn-outline-success ml-2">Create</button>
+        </form>
       </div>
     </div>
   )
